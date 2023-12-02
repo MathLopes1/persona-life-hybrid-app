83a783e68375fd20d7d614ba89dde219b8317d4a
MathLopes1 <iamatheuslopes@gmail.com>
2023-12-02 12:26:36 -0300

    chore(package-json): configurando dns de deploy :art:

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -17,7 +17,7 @@ exports.config = {
   },
   directConnect: true,
   SELENIUM_PROMISE_MANAGER: false,
-  baseUrl: 'http://localhost:4200/',
+  baseUrl: 'persona-life-app-production.up.railway.app',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
