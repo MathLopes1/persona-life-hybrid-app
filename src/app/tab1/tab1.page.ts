b044d79a7bbd548dc78f44d2d1d5c1dab2a9626b
MathLopes1 <iamatheuslopes@gmail.com>
2023-12-03 01:11:39 -0300

    fix(style): ajeitando estilos input :bug:

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
-  constructor() {}
+  name: any;
 
+  constructor(private userService: UserService) {}
+  ngOnInit() {
+    this.loadUserData();
+  }
+
+  loadUserData() {
+    const userId = localStorage.getItem('customerId');
+
+    if (userId) {
+      this.userService.findUserInformationById(userId).subscribe(
+        (data: any) => {
+          this.name = data.name.split(' ')[0];
+        },
+        (error) => {
+          console.error('Error find data user:', error);
+        }
+      );
+    }
+  }
 }
